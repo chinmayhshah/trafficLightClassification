528efce5557df8954a638cb81d8fb606abc532c0
chinmayhshah <chinmayhshah1989@gmail.com>
2017-04-26 17:09:54 -0600

    Cleaning input argument

--- cropimage.py
+++ cropimage.py
@@ -15,8 +15,10 @@ def print_image(title, filename):
     cv2.waitKey(0)
 
 
-def mask_image():
-    image = parse_args()
+def mask_image(imageName):
+    #image = parse_args(imageName)
+    image = cv2.imread(imageName)
+    #image=imageName
     r =  [([17, 15, 90], [50, 56, 200])]
     b =  [([90, 31, 4], [220, 88, 50])]
     for (lower, upper) in r:
@@ -29,8 +31,8 @@ def mask_image():
         return masked_image
 
 
-def find_contours():
-    im = mask_image()
+def find_contours(imageName):
+    im = mask_image(imageName)
     imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(imgray, 127, 255, 0)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -40,14 +42,22 @@ def find_contours():
 
 
 def crop_image(imageName):
-    output = find_contours()
+    find_contours(imageName)
     #*****#Replace the image that you want to draw on ******
     img = cv2.imread(imageName,0)
     QR_orig = cv2.imread('final.png', 0)
     QR = cv2.imread('final.png', 0)
     mask = np.zeros(QR.shape, np.uint8)
+
+    #blurred = cv2.GaussianBlur(QR, (5, 5), 0)
+    #gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+    #lab = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
+    #thresh = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)[1]
+    #cv2.imshow("Thresh", thresh)
     contours, hierarchy = cv2.findContours(QR, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     QR_final = 0
+
+
     #print "The contours are" , contours[1]
     for cnt in contours:
         if cv2.contourArea(cnt) > 100:
